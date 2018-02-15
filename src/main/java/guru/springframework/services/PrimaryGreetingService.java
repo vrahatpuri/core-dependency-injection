2a202a9f36ccb39c021548dfdf8c3543ebbd2cda
Viren <vrahatpuri@yahoo.com>
2018-02-15 06:20:31 -0700

    Usage of spring profiles, a spanish spring profile was created in this example and new set of property was added on the application.properties file to show which profile should be used.

--- src/main/java/guru/springframework/services/PrimaryGreetingService.java
+++ src/main/java/guru/springframework/services/PrimaryGreetingService.java
@@ -1,10 +1,12 @@
 package guru.springframework.services;
 
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
 @Primary
+@Profile("en")
 public class PrimaryGreetingService implements GreetingService {
     @Override
     public void sayGreeting() {
